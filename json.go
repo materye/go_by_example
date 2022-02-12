abc5652de460e0be8045582339b095baf41ca99c
Eugen Matery <eugen.matery@mailbox.org>
2022-02-12 12:19:57 +0100

    - more examples

---
+++ json.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type response1 struct {
+	Page   int
+	Fruits []string
+}
+
+type response2 struct {
+	Page   int      `json:"page"`
+	Fruits []string `json:"fruits"`
+}
+
+func main() {
+	bolB, _ := json.Marshal(true)
+	fmt.Println(string(bolB))
+
+	intB, _ := json.Marshal(1)
+	fmt.Println(string(intB))
+
+	fltB, _ := json.Marshal(2.34)
+
+}
---
+++ string-functions.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	s "strings"
+)
+
+var p = fmt.Println
+
+func main() {
+	p("Contains:	", s.Contains("test", "es"))
+	p("Count:		", s.Count("test", "t"))
+	p("HasPrefix:	", s.HasPrefix("test", "te"))
+}
